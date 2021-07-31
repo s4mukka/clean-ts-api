9f91d1d94dc31b542a2f9215056ec2c111549a61
Samuel Pereira <meninosam197@gmail.com>
2021-07-31 12:29:31 -0300

    refactor: remove code duplication

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,16 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/presentation/protocols/**',
+    '!<rootDir>/src/presentation/controllers/signup/protocols.ts'
+  ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
     '.*\\.ts$': 'ts-jest'
   },
   collectCoverageFrom: [
-    "!src/presentation/protocols/**",
-    "!src/presentation/controllers/signup/protocols.ts"
+    
   ],
 }
