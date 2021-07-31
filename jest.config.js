495b147a994c234cfc9954c4260637f6933614c3
Samuel Pereira <meninosam197@gmail.com>
2021-07-31 12:25:39 -0300

    chore: add jest configs and scripts

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,9 @@ module.exports = {
   testEnvironment: 'node',
   transform: {
     '.*\\.ts$': 'ts-jest'
-  }
+  },
+  collectCoverageFrom: [
+    "!src/presentation/protocols/**",
+    "!src/presentation/controllers/signup/protocols.ts"
+  ],
 }
