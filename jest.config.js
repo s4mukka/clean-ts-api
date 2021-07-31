4a7b3f2a016eb04ac56bad1a6af82566a80985c8
Samuel Pereira <meninosam197@gmail.com>
2021-07-31 19:57:23 -0300

    chore: add main layer to jest ignore on coverage

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,11 @@ module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/@types/**',
     '!<rootDir>/src/presentation/protocols/**',
-    '!<rootDir>/src/presentation/controllers/signup/protocols.ts'
+    '!<rootDir>/src/presentation/controllers/signup/protocols.ts',
+    '!<rootDir>/src/data/useCase/addAccount/protocols.ts'
   ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
