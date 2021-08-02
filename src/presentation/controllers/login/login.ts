3c44f79de837079a980fb98edd433c219a065dd1
Samuel Pereira <meninosam197@gmail.com>
2021-08-02 16:56:35 -0300

    feat: ensure LoginController returns 200 if valid credentials are provided

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,5 +1,5 @@
 import { InvalidParamError, MissingParamError } from '../../errors'
-import { badRequest, serverError, unauthorized } from '../../helpers/httpHelper'
+import { badRequest, ok, serverError, unauthorized } from '../../helpers/httpHelper'
 import { EmailValidator, Authentication, Controller, HttpRequest, HttpResponse } from './protocols'
 
 export class LoginController implements Controller {
@@ -34,6 +34,8 @@ export class LoginController implements Controller {
       if (!accessToken) {
         return unauthorized()
       }
+
+      return ok({ accessToken })
     } catch (error) {
       return serverError(error)
     }
