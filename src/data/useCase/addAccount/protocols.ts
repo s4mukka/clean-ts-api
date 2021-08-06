efb554c27f7df93b2b7342990daf578e7d89a1a4
Samuel Pereira <meninosam197@gmail.com>
2021-08-06 10:34:02 -0300

    feat: ensure DbAuthentication calls HashComparer with correct values

--- src/data/useCase/addAccount/protocols.ts
+++ src/data/useCase/addAccount/protocols.ts
@@ -1,4 +1,4 @@
 export * from '../../../domain/models/account'
 export * from '../../../domain/useCases/addAccount'
-export * from '../../protocols/encrypter/encrypter'
+export * from '../../protocols/criptography/encrypter'
 export * from '../../protocols/db/addAccountRepository'
