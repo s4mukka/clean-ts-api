873f68beff463185c77cbec461bac1338fa505d5
Samuel Pereira <meninosam197@gmail.com>
2021-08-08 11:10:36 -0300

    test: ensure AccountMongoRepository update the account accessToken on updateAccessToken success

--- src/infra/db/mongodb/accountRepository/account.spec.ts
+++ src/infra/db/mongodb/accountRepository/account.spec.ts
@@ -59,4 +59,18 @@ describe('Account Mongo Repository', () => {
 
     expect(account).toBeNull()
   })
+
+  test('Should update the account accessToken on updateAccessToken success', async () => {
+    const sut = makeSut()
+    const { ops: [fakeAccount] } = await accountCollection.insertOne({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+    expect(fakeAccount.accessToken).toBeFalsy()
+    await sut.updateAccessToken(fakeAccount._id, 'any_token')
+    const account = await accountCollection.findOne({ _id: fakeAccount._id })
+    expect(account).toBeTruthy()
+    expect(account.accessToken).toBe('any_token')
+  })
 })
