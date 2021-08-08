3e6186d728012a743a148b219f07ccd60f85d981
Samuel Pereira <meninosam197@gmail.com>
2021-08-08 14:57:06 -0300

    refactor: rename signup routes with authentication routes

--- src/main/routes/authenticationRoutes.test.ts
+++ src/main/routes/authenticationRoutes.test.ts
@@ -3,7 +3,7 @@ import request from 'supertest'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongoHelper'
 import app from '../config/app'
 
-describe('SignUp Routes', () => {
+describe('Authentication Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
   })
@@ -17,15 +17,17 @@ describe('SignUp Routes', () => {
     await accountCollection.deleteMany({})
   })
 
-  test('Should return an account on success', async () => {
-    await request(app)
-      .post('/api/signup')
-      .send({
-        name: 'Samuel',
-        email: 'samuel.pereira@catijr.com.br',
-        password: '123',
-        passwordConfirmation: '123'
-      })
-      .expect(200)
+  describe('POST /signup', () => {
+    test('Should return 200 on signup', async () => {
+      await request(app)
+        .post('/api/signup')
+        .send({
+          name: 'Samuel',
+          email: 'samuel.pereira@catijr.com.br',
+          password: '123',
+          passwordConfirmation: '123'
+        })
+        .expect(200)
+    })
   })
 })
