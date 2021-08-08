397242c727ce99b68b55e88e308b41ea84e234c7
Samuel Pereira <meninosam197@gmail.com>
2021-08-08 11:01:21 -0300

    feat: ensure AccountMongoRepository returns null if loadByEmail fails

--- src/infra/db/mongodb/accountRepository/account.ts
+++ src/infra/db/mongodb/accountRepository/account.ts
@@ -16,6 +16,6 @@ export class AccountMongoRepository implements AddAccountRepository, LoadAccount
     const accountCollection = await MongoHelper.getCollection('accounts')
     const account = await accountCollection.findOne({ email })
 
-    return MongoHelper.map(account)
+    return account && MongoHelper.map(account)
   }
 }
