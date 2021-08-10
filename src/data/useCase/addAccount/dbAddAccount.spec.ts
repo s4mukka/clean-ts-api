d07973b183dd4f389593a750e8257267bbc81ca0
Samuel Pereira <meninosam197@gmail.com>
2021-08-10 10:05:22 -0300

    test: ensure DbAddAccount returns null if LoadAccountByEmailRepository returns an account

--- src/data/useCase/addAccount/dbAddAccount.spec.ts
+++ src/data/useCase/addAccount/dbAddAccount.spec.ts
@@ -27,7 +27,7 @@ const makeAddAccountRepositoryStub = (): AddAccountRepository => {
 const makeLoadAccountByEmailRepository = (): LoadAccountByEmailRepository => {
   class LoadAccountByEmailRepositoryStub implements LoadAccountByEmailRepository {
     async loadByEmail (email: string): Promise<AccountModel> {
-      return new Promise(resolve => resolve(makeFakeAccount()))
+      return new Promise(resolve => resolve(null))
     }
   }
 
@@ -120,5 +120,14 @@ describe('DbAddAccount UseCase', () => {
     await sut.add(makeFakeAccountData())
 
     expect(loadSpy).toHaveBeenCalledWith('valid_email@mail.com')
+  })
+
+  test('Should return null if LoadAccountByEmailRepository not return null', async () => {
+    const { sut, loadAccountByEmailRepositoryStub } = makeSut()
+    jest.spyOn(loadAccountByEmailRepositoryStub, 'loadByEmail').mockReturnValueOnce(new Promise(resolve => resolve(makeFakeAccount())))
+
+    const account = await sut.add(makeFakeAccountData())
+
+    expect(account).toBeNull()
   })
 })
