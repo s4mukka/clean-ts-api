275f66ebf75c64498bfa8f1bcb798013f72f011a
Samuel Pereira <meninosam197@gmail.com>
2021-08-13 14:35:50 -0300

    test: ensure SaveSurveyResultController returns 500 if LoadSurveyById throws

--- src/presentation/controllers/surveyResult/saveSurveyResult.spec.ts
+++ src/presentation/controllers/surveyResult/saveSurveyResult.spec.ts
@@ -1,5 +1,5 @@
 import { InvalidParamError } from '@/presentation/errors'
-import { forbidden } from '@/presentation/helpers/http/httpHelper'
+import { forbidden, serverError } from '@/presentation/helpers/http/httpHelper'
 import { HttpRequest, SurveyModel, LoadSurveyById } from './protocols'
 import { SaveSurveyResultController } from './saveSurveyResult'
 
@@ -57,5 +57,12 @@ describe('SaveSurveyResult Controller', () => {
     jest.spyOn(loadSurveyByIdStub, 'loadById').mockReturnValueOnce(new Promise(resolve => resolve(null)))
     const httpResponse = await sut.handle(makeFakeRequest())
     expect(httpResponse).toEqual(forbidden(new InvalidParamError('surveyId')))
+  })
+
+  test('Should return 500 if LoadSurveyById throws', async () => {
+    const { sut, loadSurveyByIdStub } = makeSut()
+    jest.spyOn(loadSurveyByIdStub, 'loadById').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(serverError(new Error()))
   })
 })
