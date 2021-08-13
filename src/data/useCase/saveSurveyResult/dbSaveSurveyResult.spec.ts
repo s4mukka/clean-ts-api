caf9d3e268edc48328101ba69cb80fd30c2faf97
Samuel Pereira <meninosam197@gmail.com>
2021-08-13 10:38:12 -0300

    test: ensure DbSaveSurveyResult calls SaveSurveyResultRepository with correct values

---
+++ src/data/useCase/saveSurveyResult/dbSaveSurveyResult.spec.ts
@@ -0,0 +1,60 @@
+import MockDate from 'mockdate'
+import { SaveSurveyResultRepository, SurveyResultModel, SaveSurveyResultModel } from './protocols'
+import { DbSaveSurveyResult } from './dbSaveSurveyResult'
+
+const makeFakeSurveyResult = (): SurveyResultModel => ({
+  id: 'any_id',
+  accountId: 'any_account_id',
+  surveyId: 'any_survey_id',
+  answer: 'any_answer',
+  date: new Date()
+})
+
+const makeFakeSurveyResultData = (): SaveSurveyResultModel => ({
+  accountId: 'any_account_id',
+  surveyId: 'any_survey_id',
+  answer: 'any_answer',
+  date: new Date()
+})
+
+const makeSaveSurveyResultRepository = (): SaveSurveyResultRepository => {
+  class SaveSurveyResultRepositoryStub implements SaveSurveyResultRepository {
+    async save (data: SaveSurveyResultModel): Promise<SurveyResultModel> {
+      return new Promise(resolve => resolve(makeFakeSurveyResult()))
+    }
+  }
+
+  return new SaveSurveyResultRepositoryStub()
+}
+
+type SutTypes = {
+  sut: DbSaveSurveyResult
+  saveSurveyResultRepositoryStub: SaveSurveyResultRepository
+}
+
+const makeSut = (): SutTypes => {
+  const saveSurveyResultRepositoryStub = makeSaveSurveyResultRepository()
+  const sut = new DbSaveSurveyResult(saveSurveyResultRepositoryStub)
+
+  return {
+    sut,
+    saveSurveyResultRepositoryStub
+  }
+}
+
+describe('DbSaveSurveyResult UseCase', () => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+
+  afterAll(() => {
+    MockDate.reset()
+  })
+
+  test('Should call SaveSurveyResultRepository with correct values', async () => {
+    const { sut, saveSurveyResultRepositoryStub } = makeSut()
+    const saveSpy = jest.spyOn(saveSurveyResultRepositoryStub, 'save')
+    await sut.save(makeFakeSurveyResultData())
+    expect(saveSpy).toHaveBeenCalledWith(makeFakeSurveyResultData())
+  })
+})
