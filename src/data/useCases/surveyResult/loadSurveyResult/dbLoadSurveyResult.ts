372e55bc0416dd1c10c36074d744c2768221c39b
Samuel Pereira <meninosam197@gmail.com>
2021-08-15 15:57:23 -0300

    feat: ensure DbLoadSurveyResult calls LoadSUrveyByIdRepo if LoadSUrveyResultRepo returns null

--- src/data/useCases/surveyResult/loadSurveyResult/dbLoadSurveyResult.ts
+++ src/data/useCases/surveyResult/loadSurveyResult/dbLoadSurveyResult.ts
@@ -1,10 +1,14 @@
-import { LoadSurveyResultRepository, SurveyResultModel, LoadSurveyResult } from './protocols'
+import { LoadSurveyResultRepository, SurveyResultModel, LoadSurveyResult, LoadSurveyByIdRepository } from './protocols'
 
 export class DbLoadSurveyResult implements LoadSurveyResult {
-  constructor (private readonly loadSurveyResultRepository: LoadSurveyResultRepository) {}
+  constructor (
+    private readonly loadSurveyResultRepository: LoadSurveyResultRepository,
+    private readonly loadSurveyByIdRepository: LoadSurveyByIdRepository
+  ) {}
 
   async load (surveyId: string): Promise<SurveyResultModel> {
     const surveyResult = await this.loadSurveyResultRepository.loadBySurveyId(surveyId)
+    await this.loadSurveyByIdRepository.loadById(surveyId)
     return surveyResult
   }
 }
--- src/data/useCases/surveyResult/loadSurveyResult/protocols.ts
+++ src/data/useCases/surveyResult/loadSurveyResult/protocols.ts
@@ -1,3 +1,4 @@
 export * from '@/data/protocols/db/surveyResult/loadSurveyResultRepository'
 export * from '@/domain/models/surveyResult'
 export * from '@/domain/useCases/surveyResult/loadSurveyResult'
+export * from '@/data/protocols/db/survey/loadSurveyByIdRepository'
