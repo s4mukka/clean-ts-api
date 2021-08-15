ab4a3bbe55b89af204bda787812d67e63c144eae
Samuel Pereira <meninosam197@gmail.com>
2021-08-15 17:02:19 -0300

    feat: ensure LoadSurveyResultController returns 200 on success

--- src/presentation/controllers/surveyResult/loadSurveyResult/loadSurveyResult.ts
+++ src/presentation/controllers/surveyResult/loadSurveyResult/loadSurveyResult.ts
@@ -1,6 +1,6 @@
 
 import { InvalidParamError } from '@/presentation/errors'
-import { forbidden, serverError } from '@/presentation/helpers/http/httpHelper'
+import { forbidden, ok, serverError } from '@/presentation/helpers/http/httpHelper'
 import { Controller, HttpRequest, HttpResponse, LoadSurveyById, LoadSurveyResult } from './protocols'
 
 export class LoadSurveyResultController implements Controller {
@@ -12,9 +12,12 @@ export class LoadSurveyResultController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const { surveyId } = httpRequest.params
-      await this.loadSurveyById.loadById(surveyId)
-      await this.loadSurveyResult.load(surveyId)
-      return forbidden(new InvalidParamError('surveyId'))
+      const survey = await this.loadSurveyById.loadById(surveyId)
+      if (!survey) {
+        return forbidden(new InvalidParamError('surveyId'))
+      }
+      const surveyResult = await this.loadSurveyResult.load(surveyId)
+      return ok(surveyResult)
     } catch (error) {
       return serverError(error)
     }
