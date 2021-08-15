c953cccf84b7362d2460b5e88982c13b6413015d
Samuel Pereira <meninosam197@gmail.com>
2021-08-15 12:43:01 -0300

    test: ensure DbLoadSurveyResult calls LoadSUrveyResultRepository with correct surveyId

---
+++ src/data/useCases/surveyResult/loadSurveyResult/dbLoadSurveyResult.spec.ts
@@ -0,0 +1,28 @@
+import { LoadSurveyResultRepositoryStub } from '@/data/test'
+import faker from 'faker'
+import { DbLoadSurveyResult } from './dbLoadSurveyResult'
+
+type SutTypes = {
+  sut: DbLoadSurveyResult
+  loadSurveyResultRepositoryStub: LoadSurveyResultRepositoryStub
+}
+
+const makeSut = (): SutTypes => {
+  const loadSurveyResultRepositoryStub = new LoadSurveyResultRepositoryStub()
+  const sut = new DbLoadSurveyResult(loadSurveyResultRepositoryStub)
+
+  return {
+    sut,
+    loadSurveyResultRepositoryStub
+  }
+}
+
+describe('DbLoadSurveyResult UseCase', () => {
+  test('Should call LoadSurveyResultRepository with correct surveyId', async () => {
+    const { sut, loadSurveyResultRepositoryStub } = makeSut()
+    const loadBySurveyIdSpy = jest.spyOn(loadSurveyResultRepositoryStub, 'loadBySurveyId')
+    const surveyId = faker.datatype.uuid()
+    await sut.load(surveyId)
+    expect(loadBySurveyIdSpy).toHaveBeenCalledWith(surveyId)
+  })
+})
