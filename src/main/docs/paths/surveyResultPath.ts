9f969a40a83b1e85beba7643dd4671f8c207c904
Samuel Pereira <meninosam197@gmail.com>
2021-08-15 17:45:03 -0300

    docs: add LoadSurveyResult api docs

--- src/main/docs/paths/surveyResultPath.ts
+++ src/main/docs/paths/surveyResultPath.ts
@@ -44,5 +44,42 @@ export const surveyResultPath = {
         $ref: '#/components/serverError'
       }
     }
+  },
+  get: {
+    security: [
+      {
+        apiKeyAuth: []
+      }
+    ],
+    tags: ['Survey'],
+    summary: 'Rota para consultar o resultado de uma enquete',
+    parameters: [
+      {
+        in: 'path',
+        name: 'surveyId',
+        required: true,
+        schema: {
+          stype: 'string'
+        }
+      }
+    ],
+    responses: {
+      200: {
+        description: 'Sucesso',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/surveyResult'
+            }
+          }
+        }
+      },
+      403: {
+        $ref: '#/components/forbidden'
+      },
+      500: {
+        $ref: '#/components/serverError'
+      }
+    }
   }
 }
